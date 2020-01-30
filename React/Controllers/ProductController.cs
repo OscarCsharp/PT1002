2c38b7049eb8704ad604b431e6645b35d2f6ba97
Oscar Csharp <Oscarndlovu01@gmail.com>
2020-01-30 16:35:06 +0200

    API within React Project Complete

---
+++ React/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using React.DataLayers;
+using React.Models;
+
+namespace React.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        ProductLayer objproduct = new ProductLayer();
+
+        [HttpGet]
+        [Route("api/Product/Index")]
+        public IEnumerable<Products> Index()
+        {
+            return objproduct.GetAllProducts();
+        }
+        [HttpPost]
+        [Route("api/Product/Create")]
+        public int Create(Products product)
+        {
+            return objproduct.AddProduct(product);
+        }
+        [HttpGet]
+        [Route("api/Product/Details/{id}")]
+        public Products Details(int id)
+        {
+            return objproduct.GetProductData(id);
+        }
+        [HttpPut]
+        [Route("api/Product/Edit")]
+        public int Edit(Products product)
+        {
+            return objproduct.UpdateProduct(product);
+        }
+        [HttpDelete]
+        [Route("api/Product/Delete/{id}")]
+        public string Delete(int id)
+        {
+            return objproduct.DeleteProduct(id);
+        }
+       
+
+
+    }
+}
---
+++ React/Models/OrderDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace React.Models
+{
+    public partial class OrderDetails
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+
+        public virtual Products Product { get; set; }
+    }
+}
